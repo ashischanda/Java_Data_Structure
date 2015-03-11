06aeb8049eb3429fb4a3c28e094c318d4f0d822e
Ashis Kumar Chanda <ashis415@yahoo.com>
2015-03-11 20:39:12 +0600

    Create Node.java

---
+++ linkedList/Node.java
@@ -0,0 +1,11 @@
+public class Node{
+  public Object element;
+  public Node next; // it should be null or point to next
+  
+  public Node(Object e, Node n){
+    element =e ;
+    next = n;
+    
+  }
+  
+}
